3e0c9e6f9ca6187f562ea34edf4e02861919c096
Yara Mohamed <myara1405@gmail.com>
2025-09-27 10:45:50 +0300

    feat(dashboard): fetch user statistics and list of bought & rented properties

--- app/Http/Controllers/OwnerDashboardController.php
+++ app/Http/Controllers/OwnerDashboardController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use App\Models\Property;
+use App\Models\PropertyPurchase;
+use App\Models\Purchase;
 use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -30,6 +32,48 @@ class OwnerDashboardController extends Controller
         return response()->json([
             'message' => 'Owner Dashboard Data',
             'data'    => $data,
+        ]);
+    }
+    public function stats(){
+        $user = auth('api')->user();
+
+        $total_properties = Property::where('owner_id', $user->id)->count();
+
+        $bookingsCount = Purchase::whereHas('property', function ($q) use ($user) {
+            $q->where('owner_id', $user->id);
+        })->where('status', 'successful') ->count();
+
+
+        $salesIncome = PropertyPurchase::where('seller_id', $user->id)
+            ->where('status', 'paid')
+            ->sum('amount');
+
+        $rentIncome = Purchase::whereHas('property', function ($q) use ($user) {
+            $q->where('owner_id', $user->id);
+        })->where('status', 'successful')
+          ->sum('amount');
+
+        $totalIncome = $salesIncome + $rentIncome;
+        
+        $boughtProperties = PropertyPurchase::with('property')
+            ->where('buyer_id', $user->id)
+            ->get();
+
+        $rentedProperties = Purchase::with('property')
+            ->where('user_id', $user->id)
+            ->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'total_properties'=>$total_properties,
+                'bookings_count' => $bookingsCount,
+                'sales_income'   => $salesIncome,
+                'rent_income'    => $rentIncome,
+                'total_income'   => $totalIncome,
+                'bought_properties' => $boughtProperties,
+                'rented_properties' => $rentedProperties,
+            ]
         ]);
     }
 }
